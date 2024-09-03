423e0caa7bd64572ff1ac61bffeecbf486cd0bc6
test <test@test-ka-MacBook-Pro.local>
2024-09-04 01:57:17 +0530

    backup

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,22 @@ mod request;
 async fn echo(body: web::Bytes) -> Result<HttpResponse, Error> {
     // body is loaded, now we can deserialize serde-json
     let obj = serde_json::from_slice::<request::Request>(&body)?;
-    Ok(HttpResponse::Ok().json(obj)) // <- send response
+    Ok(HttpResponse::Ok().insert_header(("x-jacob-test", "value")).json(obj)) // <- send response
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-    tracerust::init_tracer();
-    log::info!("starting HTTP server at http://localhost:5001");
+    rustagent::init_tracer();
+    log::info!("starting HTTP server at port 5001");
 
     HttpServer::new(|| {
         App::new()
-            .wrap(tracerust::TraceRust)
+            .wrap(rustagent::RustAgent::default())
             // enable logger
             .wrap(middleware::Logger::default())
             .service(web::resource("/echo").route(web::post().to(echo)))    })
-    .bind(("127.0.0.1", 5001))?
+    .bind(("0.0.0.0", 5001))?
     .run()
     .await
 }
